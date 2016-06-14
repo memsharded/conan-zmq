68408fd47329b00f009d8249b7ddb8ba773323fb
memsharded <james@conan.io>
2016-06-15 00:47:39 +1000

    forgot to rename build.py

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="memsharded", channel="testing")
-    builder.add_common_builds(shared_option_name="ZMQ:shared")
+    builder.add_common_builds(shared_option_name="libzmq:shared")
     builder.run()
