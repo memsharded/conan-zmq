584d6dc1c5590203ac467978980d314a368c28ed
memsharded <james@conan.io>
2017-05-18 14:28:34 +0200

    version win

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ conan_basic_setup()
             ver = ""
             if self.settings.compiler == "Visual Studio":
                 v = str(self.settings.compiler.version)
-                ver = "-v141" if v == "15" else ("-v%s" % v)
+                ver = "-v141" if v == "15" else ("-v%s0" % v)
             stat_fix = "s" if not self.options.shared else ""
             debug_fix = "gd" if self.settings.build_type == "Debug" else ""
             fix = ("-%s%s" % (stat_fix, debug_fix)) if stat_fix or debug_fix else ""
