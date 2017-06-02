d9cf2de119509ddce491c1263e1ce2e6810c0b21
memsharded <james@conan.io>
2017-06-02 14:59:31 +0200

    named pages for travis

--- build.py
+++ build.py
@@ -7,7 +7,13 @@ if __name__ == "__main__":
     
     named_builds = {}
     for settings, options, env_vars, build_requires in builder.builds:
-        named_build = named_builds.setdefault("%s_%s_%s" % (settings["compiler"].replace(" ", ""), settings["compiler.version"], settings["arch"]), [])
+        if settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10" and settings["arch"] == "x86_64":
+            continue
+        if settings["compiler"] == "gcc":
+            name = "%s_%s" % (settings["compiler"], settings["compiler.version"].replace(".", ""))
+        elif settings["compiler"] == "Visual Studio":
+            name = "%s_%s_%s" % (settings["compiler"].replace(" ", ""), settings["compiler.version"], settings["arch"])
+        named_build = named_builds.setdefault(name, [])
         named_build.append([settings, options, env_vars, build_requires])
     builder.builds = []
     builder.named_builds = named_builds
