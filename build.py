cdcd188aa9c03c138ca53254e0e3f995f8a86094
memsharded <james@conan.io>
2017-06-02 12:29:25 +0200

    new named pages

--- build.py
+++ build.py
@@ -4,4 +4,12 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="memsharded", channel="testing", visual_versions=["10", "12", "14", "15"])
     builder.add_common_builds(shared_option_name="libzmq:shared")
+    
+    named_builds = {}
+    for settings, options, env_vars, build_requires in builder.builds:
+        named_build = named_builds.set_default("%s_%s_%s" % (settings["compiler"].replace(" ", ""), settings["compiler.version"], settings["arch"]), [])
+        named_build.append([settings, options, env_vars, build_requires])
+    builder.builds = []
+    builder.named_builds = named_builds
+
     builder.run()
