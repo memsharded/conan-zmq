453ae9ee0554d43bc85b38ef79b3b8c834fc6aa3
memsharded <james@conan.io>
2017-06-05 09:24:26 +0200

    trying to publish to stable

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="testing", visual_versions=["10", "12", "14", "15"])
+    builder = ConanMultiPackager(username="memsharded", channel="stable", visual_versions=["10", "12", "14", "15"])
     builder.add_common_builds(shared_option_name="libzmq:shared")
     
     named_builds = {}
